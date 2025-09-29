69c4b0dff1d4d18d928c8b5b454f9ff1a35fbb11
elijah <cuiyeqing960904@gmail.com>
2025-09-29 23:49:25 +0300

    config settings

--- store/users_test.go
+++ store/users_test.go
@@ -1,2 +1,20 @@
 package store
 
+import (
+	"go-sqs/config"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestUserStore(t *testing.T) {
+	os.Setenv("ENV", string(config.Env_Test))
+
+	conf, err := config.New()
+	require.NoError(t, err)
+
+	db, err := NewPostgresDB(conf)
+	require.NoError(t, err)
+	defer db.Close()
+}
