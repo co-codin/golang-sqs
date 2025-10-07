4939970cc1e2fb381fa251b2dcd3e2635715368a
elijah <cuiyeqing960904@gmail.com>
2025-10-07 23:01:13 +0300

    aws test working

--- cmd/awstest/main.go
+++ cmd/awstest/main.go
@@ -8,18 +8,28 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 )
 
 func main() {
 	ctx := context.Background()
-	sdkConfig, err := config.LoadDefaultConfig(ctx)
+	sdkConfig, err := config.LoadDefaultConfig(ctx,
+		config.WithRegion("us-east-1"),
+		config.WithCredentialsProvider(credentials.StaticCredentialsProvider{
+			Value: aws.Credentials{
+				AccessKeyID:     "key",
+				SecretAccessKey: "secret",
+			},
+		}),
+	)
 	if err != nil {
 		fmt.Println("couldn't load default config")
 		fmt.Println(err)
 		return
 	}
 	s3Client := s3.NewFromConfig(sdkConfig, func(options *s3.Options) {
-		options.BaseEndpoint = aws.String("http://s3.localhost.localstack.cloud:4566")
+		options.BaseEndpoint = aws.String("http://localhost:4566")
+		options.UsePathStyle = true
 	})
 	out, err := s3Client.ListBuckets(ctx, &s3.ListBucketsInput{})
 	if err != nil {
@@ -27,6 +37,6 @@ func main() {
 	}
 
 	for _, bucket := range out.Buckets {
-		fmt.Println(bucket.Name)
+		fmt.Println(*bucket.Name)
 	}
 }
