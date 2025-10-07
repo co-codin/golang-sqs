587b6822d480975c8f001f1ed26e1b4a9aa44c79
elijah <cuiyeqing960904@gmail.com>
2025-10-08 00:49:36 +0300

    report test

--- store/reports_test.go
+++ store/reports_test.go
@@ -1,1 +1,32 @@
 package store_test
+
+import (
+	"context"
+	"go-sqs/fixtures"
+	"go-sqs/store"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestReportStore(t *testing.T) {
+	env := fixtures.NewTestEnv(t)
+	cleanup := env.SetupDb(t)
+	t.Cleanup(func() {
+		cleanup(t)
+	})
+
+	ctx := context.Background()
+	reportStore := store.NewReportStore(env.DB)
+	userStore := store.NewUserStore(env.DB)
+	user, err := userStore.CreateUser(ctx, "test@test.com", "secretpassword")
+	require.NoError(t, err)
+
+	now := time.Now()
+	report, err := reportStore.Create(ctx, user.Id, "monsters")
+	require.NoError(t, err)
+	require.Equal(t, user.Id, report.UserID)
+	require.Equal(t, "monsters", report.ReportType)
+	require.Less(t, now.UnixNano(), report.CreatedAt.UnixNano())
+}
