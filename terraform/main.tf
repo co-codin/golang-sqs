33699a0a75097d56e7b762d2543af125ab94a48b
elijah <cuiyeqing960904@gmail.com>
2025-10-07 21:50:15 +0300

    terraform added

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,28 @@
+variable "aws_secret_access_key" {
+  type        = string
+}
+
+variable "aws_access_key_id" {
+  type        = string
+}
+
+variable "aws_default_region" {
+  type        = string
+}
+
+variable "s3_bucket" {
+  type        = string
+}
+
+variable "sqs_queue" {
+  type        = string
+}
+
 provider "aws" {
 
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
-  region                      = "us-east-1"
-
+  access_key                  = var.aws_access_key_id
+  secret_key                  = var.aws_secret_access_key
+  region                      = var.aws_default_region
   s3_use_path_style           = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
@@ -14,6 +33,14 @@ provider "aws" {
   }
 }
 
-resource "aws_s3_bucket" "test-bucket" {
-  bucket = "my-bucket"
+resource "aws_s3_bucket" "reports-s3-bucket" {
+  bucket = var.s3_bucket
+}
+
+resource "aws_sqs_queue" "terraform_queue" {
+  name                      = var.sqs_queue
+  delay_seconds             = 5
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
 }
